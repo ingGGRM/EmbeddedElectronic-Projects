feb820f2a4a8fc399c0498f63a24fc6efeac4958
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2023-03-14 11:33:17 -0400

    Fixed variable declarations missing

--- autoFilledWaterCooler-ReverseEngineering/autoFilledWaterCooler-ReverseEngineering.ino
+++ autoFilledWaterCooler-ReverseEngineering/autoFilledWaterCooler-ReverseEngineering.ino
@@ -1,35 +1,41 @@
 /********** INPUT DECLARATIONS **********/
-int heatSensor = A0;  // FLOAT
-int coolSensor = A1;  // TR-C
-int floatSensor = A2; // TR-H
-int switches = A3;    // SW-H&C
+int heatSensor = A0;   // FLOAT
+int coolSensor = A1;   // TR-C
+int floatSensor = A2;  // TR-H
+int switches = A3;     // SW-H&C
 
-int overSensor = A4; // FLOAT-P
-int door = A5;       // CN2
+int overSensor = A4;  // FLOAT-P
+int door = A5;        // CN2
 
-/********** OUTPUT DECLARATIONS **********/
-int ledA = 2;   // L1
-int ledB = 3;   // L2
-int ledC = 4;   // L3
-int ledD = 5;   // L4
-int pump = 6;   // PUMP
-int heater = 7; // HEAT
-int cooler = 8; // COOL
+int heatAdc;
+int coolAdc;
+int floatAdc;
+int switchesAdc;
+int overAdc;
+int doorAdc;
 
 /********** OUTPUT DECLARATIONS **********/
-boolean canHeat = true; //
-boolean canCool = true; //
-boolean canFill = true; //
+int ledA = 2;    // L1
+int ledB = 3;    // L2
+int ledC = 4;    // L3
+int ledD = 5;    // L4
+int pump = 6;    // PUMP
+int heater = 7;  // HEAT
+int cooler = 8;  // COOL
+
+/********** OUTPUT DECLARATIONS **********/
+boolean canHeat = true;  //
+boolean canCool = true;  //
+boolean canFill = true;  //
 
 /********** MAIN SETUP FUNCTION **********/
-void setup()
-{
-  pinMode(heatSensor, INPUT);  // ANALOG
-  pinMode(coolSensor, INPUT);  // ANALOG
-  pinMode(floatSensor, INPUT); // ANALOG
-  pinMode(switches, INPUT);    // ANALOG
-  pinMode(overSensor, INPUT);  // DIGITAL
-  pinMode(door, INPUT);        // DIGITAL
+void setup() {
+  pinMode(heatSensor, INPUT);   // ANALOG
+  pinMode(coolSensor, INPUT);   // ANALOG
+  pinMode(floatSensor, INPUT);  // ANALOG
+  pinMode(switches, INPUT);     // ANALOG
+  pinMode(overSensor, INPUT);   // DIGITAL
+  pinMode(door, INPUT);         // DIGITAL
 
   pinMode(ledA, OUTPUT);
   pinMode(ledB, OUTPUT);
@@ -49,14 +55,14 @@ void setup()
   digitalWrite(cooler, HIGH);
 
   /********** SET 2HZ TIMER INTERRUPT **********/
-  noInterrupts(); // disable all interrupts
+  noInterrupts();  // disable all interrupts
   TCCR1A = 0;
   TCCR1B = 0;
   // Set timer1_counter to the correct value for our interrupt interval
-  TCNT1 = 15000;          // 34286 // preload timer 65536-16MHz/256/2Hz
-  TCCR1B |= (1 << CS12);  // 256 prescaler
-  TIMSK1 |= (1 << TOIE1); // enable timer overflow interrupt
-  interrupts();           // enable all interrupts
+  TCNT1 = 15000;           // 34286 // preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);   // 256 prescaler
+  TIMSK1 |= (1 << TOIE1);  // enable timer overflow interrupt
+  interrupts();            // enable all interrupts
 
   /********** SET SERIAL MONITOR CONFIG **********/
   Serial.begin(9600);
@@ -65,37 +71,30 @@ void setup()
 }
 
 /********** MAIN LOOP FUNCTION **********/
-void loop()
-{
-  if (canCool)
-  {
-    digitalWrite(pump, HIGH); // Start Cooler
-  }
-  else
-  {
-    digitalWrite(pump, LOW); // Stop Cooler
+void loop() {
+  if (canCool) {
+    digitalWrite(pump, HIGH);  // Start Cooler
+  } else {
+    digitalWrite(pump, LOW);  // Stop Cooler
   }
 
-  if (canFill)
-  {
-    digitalWrite(ledD, HIGH); // Start Pump
-  }
-  else
-  {
-    digitalWrite(ledD, LOW); // Stop Pump
+  if (canFill) {
+    digitalWrite(ledD, HIGH);  // Start Pump
+  } else {
+    digitalWrite(ledD, LOW);  // Stop Pump
   }
 }
 
 /********** TIMER OVERFLOW INTERRUPT ISR **********/
-ISR(TIMER1_OVF_vect)
-{
-  TCNT1 = 15000; // reset timer for next loops
-  int heatAdc = analogRead(heatSensor);
-  int coolAdc = analogRead(coolSensor);
-  int floatAdc = analogRead(floatSensor);
-  int switchesAdc = analogRead(switches);
-  int overAdc = analogRead(overSensor);
-  int doorAdc = analogRead(door);
+ISR(TIMER1_OVF_vect) {
+  TCNT1 = 15000;  // reset timer for next loops
+
+  heatAdc = analogRead(heatSensor);
+  coolAdc = analogRead(coolSensor);
+  floatAdc = analogRead(floatSensor);
+  switchesAdc = analogRead(switches);
+  overAdc = analogRead(overSensor);
+  doorAdc = analogRead(door);
 
   if (coolAdc < 180 && coolAdc > 170)
     canCool = false;
@@ -107,11 +106,10 @@ ISR(TIMER1_OVF_vect)
   else if ((floatAdc > 285 && floatAdc < 295) || digitalRead(overSensor) == HIGH)
     canFill = true;
 
-  verbose(heatAdc, coolAdc, floatAdc, switchesAdc, overAdc, doorAdc);
+  verbose();
 }
 
-void verbose(heatAdc, coolAdc, floatAdc, switchesAdc, overAdc, doorAdc)
-{
+int verbose() {
   Serial.print(heatAdc);
   Serial.println(" heat");
   Serial.println();
@@ -131,24 +129,18 @@ void verbose(heatAdc, coolAdc, floatAdc, switchesAdc, overAdc, doorAdc)
   Serial.println(" door");
   Serial.println();
 
-  if (canFill)
-  {
+  if (canFill) {
     Serial.println("Can Fill");
     Serial.println();
-  }
-  else
-  {
+  } else {
     Serial.println("Can't Fill");
     Serial.println();
   }
 
-  if (canCool)
-  {
+  if (canCool) {
     Serial.println("Can Cool");
     Serial.println();
-  }
-  else
-  {
+  } else {
     Serial.println("Can't Cool");
     Serial.println();
   }
